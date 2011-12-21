f5b7ab06dae81653da6554d7cc77fdc3a390216f
Scott Coleman <scott.coleman@tee2.org>
2011-12-21 10:40:13 -0800

    Initial checkin

---
+++ GitTest3/src/com/t2/gittest4/GitTest4.java
@@ -0,0 +1,13 @@
+package com.t2.gittest4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitTest4 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
