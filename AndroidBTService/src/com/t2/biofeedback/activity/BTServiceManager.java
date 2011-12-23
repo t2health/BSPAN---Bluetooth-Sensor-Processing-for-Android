dfebb1c29b5dc0e1eec93bc097727487fd73200b
Scott Coleman <scott.coleman@tee2.org>
2011-12-23 07:55:28 -0800

    Added license headers and updated javadoc

--- AndroidBTService/src/com/t2/biofeedback/activity/BTServiceManager.java
+++ AndroidBTService/src/com/t2/biofeedback/activity/BTServiceManager.java
@@ -1,3 +1,52 @@
+/*****************************************************************
+SPINE - Signal Processing In-Node Environment is a framework that 
+allows dynamic on node configuration for feature extraction and a 
+OtA protocol for the management for WSN
+
+Copyright (C) 2007 Telecom Italia S.p.A. 
+ 
+GNU Lesser General Public License
+ 
+This library is free software; you can redistribute
+modify it under the terms of the sub-license (below).
+
+*****************************************************************/
+
+/*****************************************************************
+BSPAN - BlueTooth Sensor Processing for Android is a framework 
+that extends the SPINE framework to work on Android and the 
+Android Bluetooth communication services.
+
+Copyright (C) 2011 The National Center for Telehealth and 
+Technology
+
+Eclipse Public License 1.0 (EPL-1.0)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the Eclipse Public License as
+published by the Free Software Foundation, version 1.0 of the 
+License.
+
+The Eclipse Public License is a reciprocal license, under 
+Section 3. REQUIREMENTS iv) states that source code for the 
+Program is available from such Contributor, and informs licensees 
+how to obtain it in a reasonable manner on or through a medium 
+customarily used for software exchange.
+
+Post your updates and modifications to our GitHub or email to 
+t2@tee2.org.
+
+This library is distributed WITHOUT ANY WARRANTY; without 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE.  See the Eclipse Public License 1.0 (EPL-1.0)
+for more details.
+ 
+You should have received a copy of the Eclipse Public License
+along with this library; if not, 
+visit http://www.opensource.org/licenses/EPL-1.0
+
+*****************************************************************/
+
 package com.t2.biofeedback.activity;
 
 import java.util.ArrayList;
